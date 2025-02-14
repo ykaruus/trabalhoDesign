e95b395b20876be9fad1b67267443b6f91740bf1
ykaruus <147627870+ykaruus@users.noreply.github.com>
2025-02-13 23:24:34 -0400

    aaa
    aaa

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+document.querySelector(".button").addEventListener("click", () => {
+    document.querySelector(".fundo").classList.toggle("disabled");
+});
+
+document.querySelector(".fundo").addEventListener("click", () => {
+    document.querySelector(".fundo").classList.toggle("disabled");
+})
